0233a7f6697e1e41c4c9ff8ecd9c9d159bfff314
Glauco Cetara <neocetara@hotmail.com>
2020-11-19 08:17:24 -0300

    Comment Route Users and Add routes Products

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::apiResource('/categories', 'API\CategoryController');
+
+Route::apiResource('/products', 'API\ProductController');
