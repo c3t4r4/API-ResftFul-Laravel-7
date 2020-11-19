5fb293a0382539b4190167b5dfed933f2724a56c
Glauco Cetara <neocetara@hotmail.com>
2020-11-19 07:40:06 -0300

    Create Controller and API Methods

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        $products = Product::getResults($request->name);
+
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product;
+        $product->fill($request->all());
+
+        if(!$product->save()){
+            return response()->json(['error' => 'Not found'], 404);
+        }
+
+        return response()->json($product, 201);
+    }
+
+    public function show($product)
+    {
+        $product = Product::find($product);
+
+        if(empty($product)) {
+            return response()->json(['error' => 'Not found'], 404);
+        }
+
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $product)
+    {
+        $product = Product::find($product);
+        if(!empty($product)) {
+            $product->fill($request->all());
+
+            if (!$product->save()) {
+                return response()->json(['error' => 'Not found'], 404);
+            }
+        }
+
+        return response()->json($product);
+    }
+
+    public function destroy($product)
+    {
+        $product = Product::find($product);
+        if(!empty($product)) {
+            $product->delete();
+            return response()->json(['success' => 'register has been deleted'], 200);
+        }
+
+        return response()->json(['error' => 'Not found'], 404);
+    }
+}
