2e2a2253617a0bfe551266c3c344d1e7859b32dc
Glauco Cetara <neocetara@hotmail.com>
2020-11-19 08:15:44 -0300

    Add Fillable category_id and change method GetResults

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,14 +10,30 @@ class Product extends Model
         'name',
         'description',
         'image',
+        'category_id'
     ];
 
-    public function scopeGetResults($query, $name = null)
+    public function scopeGetResults($query, $data = null, $total = 10)
     {
-        if(!empty($name)){
-            return $query->where('name', 'LIKE', "%{$name}%")->get();
+        if(empty($data['filter']) && empty($data['name']) && empty($data['description'])){
+            return $query->paginate($total);
         }
 
-        return $query->get();
+        return $query->where(function ($row) use ($data) {
+           if(!empty($data['filter'])){
+               $filter = $data['filter'];
+               $row->where('name', $data['filter']);
+               $row->orWhere('description', 'LIKE', "%{$filter}%");
+           }
+
+            if(!empty($data['name'])){
+                $row->where('name', $data['name']);
+            }
+
+            if(!empty($data['description'])){
+                $description = $data['description'];
+                $row->where('description', 'LIKE', "%{$description}%");
+            }
+        })->paginate($total);
     }
 }
