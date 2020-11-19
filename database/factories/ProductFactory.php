e7565e1d2e165e1cb268a32e5d6b9c492c4ee4a8
Glauco Cetara <neocetara@hotmail.com>
2020-11-19 07:39:18 -0300

    Insert category_id random

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,11 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Product;
+use App\Models\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->unique()->word,
-        'description' => $faker->sentence()
+        'description' => $faker->sentence(),
+        'category_id' => Category::all()->random()->id
     ];
 });
